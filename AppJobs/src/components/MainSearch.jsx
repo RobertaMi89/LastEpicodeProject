b16e665b7080da52343af827b53facfc9d8246d6
RobertaMi89 <roberta.miosi@gmail.com>
2024-01-24 11:36:02 +0100

    commit

--- AppJobs/src/components/MainSearch.jsx
+++ AppJobs/src/components/MainSearch.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
-import { Container, Row, Col, Form } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import Job from "./Job";
 
 const MainSearch = () => {
   const [query, setQuery] = useState("");
   const [jobs, setJobs] = useState([]);
+
+  const navigate = useNavigate();
 
   const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?search=";
 
@@ -31,8 +34,11 @@ const MainSearch = () => {
   return (
     <Container>
       <Row>
-        <Col xs={10} className="mx-auto my-3">
-          <h1 className="display-1">Remote Jobs Search</h1>
+        <Col xs={10} className="d-flex flex-wrap align-items-center mx-auto my-3">
+          <h1 className="display-1 me-auto">Remote Jobs Search</h1>
+          <Button variant="outline-primary" onClick={() => navigate("/favourites")}>
+            go to Favourites
+          </Button>
         </Col>
         <Col xs={10} className="mx-auto">
           <Form onSubmit={handleSubmit}>
